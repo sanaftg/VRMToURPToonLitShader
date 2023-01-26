775fe1ffd78ddae5b136c7ac7b1a5f56a22550d7
SANA@幻想テクノロギア <sana@f-tg.net>
2023-01-27 03:44:43 +0900

    Update URPToonShaderChanger.cs

--- URPToonShaderChanger.cs
+++ URPToonShaderChanger.cs
@@ -11,14 +11,15 @@ public class URPToonShaderChanger : MonoBehaviour
         Shader shader = Shader.Find("SimpleURPToonLitExample(With Outline)");
         foreach (var obj in Selection.objects)
         {
-            if (obj.GetType() == typeof(Material)) { }
-            Material mat = (Material)obj;
-            if (mat.shader == shader) continue;
-            Texture tex = mat.mainTexture;
-            Debug.Log(tex);
-            mat.shader = shader;
-            mat.mainTexture = tex;
-            EditorUtility.SetDirty(mat);
+            if (obj.GetType() == typeof(Material)) { 
+                Material mat = (Material)obj;
+                if (mat.shader == shader) continue;
+                Texture tex = mat.mainTexture;
+                Debug.Log(tex);
+                mat.shader = shader;
+                mat.mainTexture = tex;
+                EditorUtility.SetDirty(mat);
+            }
         }
     }
 }
