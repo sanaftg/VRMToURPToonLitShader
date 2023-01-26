cf29bccba3dd752b351b16ab00ca9e334ede0f06
SANA <sana@f-tg.net>
2023-01-27 03:37:03 +0900

    Create URPToonShaderChanger.cs

---
+++ URPToonShaderChanger.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class URPToonShaderChanger : MonoBehaviour
+{
+    [MenuItem("Assets/ChangeURPToon")]
+    public static void ChangeURPToonShader()
+    {
+        Shader shader = Shader.Find("SimpleURPToonLitExample(With Outline)");
+        foreach (var obj in Selection.objects)
+        {
+            if (obj.GetType() == typeof(Material)) { }
+            Material mat = (Material)obj;
+            if (mat.shader == shader) continue;
+            Texture tex = mat.mainTexture;
+            Debug.Log(tex);
+            mat.shader = shader;
+            mat.mainTexture = tex;
+            EditorUtility.SetDirty(mat);
+        }
+    }
+}
